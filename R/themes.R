9f8ee33d6b17fc210296434846a15fab8dee3eb4
J.J. Moncus <jj.moncus@gartner.com>
2025-04-11 17:25:05 -0400

    rename to theme_main

--- R/themes.R
+++ R/themes.R
@@ -1,14 +1,14 @@
-
 #' Generic theme for Priorities Poll charts
 #'
 #' @export
 #'
 #' @importFrom ggplot2 theme element_text element_blank element_line
 #'
-theme_priorities_poll <- function(text_size = 12) {
-
-  theme(text = element_text(size = text_size),
-        axis.line = element_line(),
-        panel.background = element_blank(),
-        axis.ticks.y = element_blank())
+theme_main <- function(text_size = 12) {
+  theme(
+    text = element_text(size = text_size),
+    axis.line = element_line(),
+    panel.background = element_blank(),
+    axis.ticks.y = element_blank()
+  )
 }
